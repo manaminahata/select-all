5a26830af9e75e6e23e9c1cf9df229ad14bdf218
manaminahata <93502848+manaminahata@users.noreply.github.com>
2022-10-18 19:13:44 +0900

    全選択ボタンの実装 全選択ボタンを押下すると一括でチェックが入り、再度押下すると解除されるよう実装。

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "./styles.css";
+import $ from "jquery";
+let clickCount = 0;
+$(function () {
+  $("#select-all").click(function () {
+    clickCount += 1;
+    if (clickCount % 2 === 1) {
+      $("input[name=check]").prop("checked", true);
+    } else {
+      $("input[name=check]").prop("checked", false);
+    }
+  });
+});
